a3cf33eafe3982485d0e8efaffe4c5a1659dd915
PC <valentindylan@hotmail.fr>
2026-04-29 15:14:02 +0200

    add of buffer to filewriter

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -1,4 +1,6 @@
 package com.hemebiotech.analytics;
+
+import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
@@ -26,12 +28,12 @@ public class WriteSymptomDataToFile implements ISymptomWriter {
      * @param symptoms map of symptom names to occurrence counts
      */
     @Override
-    public void writeSymptoms(Map<String, Integer> symptoms) throws IOException 		{
+    public void writeSymptoms(Map<String, Integer> symptoms){
         if (filepath != null && symptoms != null) {
-            try (FileWriter writer = new FileWriter(filepath)) {
+            try (BufferedWriter writer = new BufferedWriter( new FileWriter(filepath))) {
                 for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
-                    writer.write(entry.getKey() + ": " + entry.getValue() + "\n");
-
+                    writer.write(entry.getKey() + ": " + entry.getValue());
+                    writer.newLine();
                 }
                 writer.close(); 
             } catch (IOException e) {
